ba65431289f5554e450e7768a5bf9188e01dbaf4
Laíssa Mendes <laissa.mendes2006@gmail.com>
2024-04-08 11:28:05 -0300

    aplicação dos estilos nos layouts

--- src/layouts/LayoutLarge.vue
+++ src/layouts/LayoutLarge.vue
@@ -5,7 +5,8 @@ import LargeHeader from '../components/templates/large/LargeHeader.vue';
 </script>
 
 <template>
-    <header>
+    <div id="layout-large">
+        <header>
         <large-header/>
     </header>
     <aside>
@@ -14,4 +15,52 @@ import LargeHeader from '../components/templates/large/LargeHeader.vue';
     <footer>
         <large-footer/>
     </footer>
+    </div>
+   
 </template>
+<style scoped>
+#layout-large {
+  display: grid;
+  grid-template-columns: 1fr 5fr;
+  grid-template-rows: auto 1fr auto;
+  grid-template-areas:
+  'aside header'
+  'aside main'
+  'aside footer';
+  min-height: 100vh;
+}
+
+header {
+    display: flex;
+    justify-content: flex-end;
+    padding: 1.5rem;
+    grid-area: header;
+}
+
+aside {
+  grid-area: aside;
+  border-right: #eeeeee 1px solid;
+  box-shadow: 0 0 10px 0 #eeeeee;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
+}
+
+main {
+  min-height: 80%;
+  padding: 2rem;
+  grid-area: main;
+}
+
+footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 3rem;
+  background-color: #eeeeee;
+  grid-area: footer;
+}
+</style>
--- src/layouts/LayoutMedium.vue
+++ src/layouts/LayoutMedium.vue
@@ -5,7 +5,8 @@ import MediumHeader from '../components/templates/medium/MediumHeader.vue';
 </script>
 
 <template>
-    <header>
+    <div id="medium-large">
+        <header>
         <medium-header/>
     </header>
     <aside>
@@ -14,4 +15,52 @@ import MediumHeader from '../components/templates/medium/MediumHeader.vue';
     <footer>
         <medium-footer/>
     </footer>
+    </div>
+   
 </template>
+<style scoped>
+#medium-large {
+  display: grid;
+  grid-template-columns: 1fr 5fr;
+  grid-template-rows: auto 1fr auto;
+  grid-template-areas:
+  'aside header'
+  'aside main'
+  'aside footer';
+  min-height: 100vh;
+}
+
+header {
+    display: flex;
+    justify-content: flex-end;
+    padding: 1.5rem;
+    grid-area: header;
+}
+
+aside {
+  grid-area: aside;
+  border-right: #eeeeee 1px solid;
+  box-shadow: 0 0 10px 0 #eeeeee;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
+}
+
+main {
+  min-height: 80%;
+  padding: 2rem;
+  grid-area: main;
+}
+
+footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 3rem;
+  background-color: #eeeeee;
+  grid-area: footer;
+}
+</style>
